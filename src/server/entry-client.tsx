9a38ceec226057491eadf5ea8afb83051597fca4
Ana Berg <ana.berg99@gmail.com>
2025-08-30 18:04:00 -0300

    fix: imports

--- src/server/entry-client.tsx
+++ src/server/entry-client.tsx
@@ -2,8 +2,6 @@ import { hydrateRoot } from 'react-dom/client';
 import { RouterClient } from '@tanstack/react-router/ssr/client';
 import { createRouter } from './router';
 
-import './index.css';
-
 const router = createRouter();
 
 hydrateRoot(document, <RouterClient router={router} />);
--- src/server/entry-server.tsx
+++ src/server/entry-server.tsx
@@ -4,8 +4,6 @@ import {
 } from '@tanstack/react-router/ssr/server';
 import { createRouter } from './router';
 
-import './index.css';
-
 export async function render({ request }: { request: Request }) {
   const handler = createRequestHandler({ request, createRouter });
   return handler(defaultRenderHandler);
