2b519e6367ff014868934b90105ed77e08a53b92
Ana Berg <ana.berg99@gmail.com>
2025-08-30 22:26:36 -0300

    feat: console on terminal

--- src/server/entry-client.tsx
+++ src/server/entry-client.tsx
@@ -3,6 +3,13 @@ import { RouterClient } from '@tanstack/react-router/ssr/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter } from './router';
 
+if (typeof window !== 'undefined') {
+  const noop = () => {};
+  console.log = noop;
+  console.info = noop;
+  console.debug = noop;
+}
+
 const queryClient = new QueryClient();
 const router = createRouter({ context: { queryClient } });
 
